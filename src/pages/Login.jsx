54fdbe56369e600512cb0412a81f40f0b7ec39f2
Gayuh Tri Rahutami <13519192@std.stei.itb.ac.id>
2021-11-26 13:44:06 +0700

    added fetch to login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,68 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
-
+import React, { useRef } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import { api_base_url } from "../config";
+import { TokenContext } from "../App";
+import { Navigate } from "react-router-dom";
 const Login = () => {
-return (
-<MDBContainer>
-  <MDBRow>
-    <MDBCol md="6">
-      <form>
-        <p className="h4 text-center mb-4">Sign in</p>
-        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-          Email
-        </label>
-        <input type="email" id="defaultFormLoginEmailEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-          Password
-        </label>
-        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" />
-        <div className="text-center mt-4">
-          <MDBBtn color="indigo" type="submit">Login</MDBBtn>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-);
+  const username = useRef("");
+  const password = useRef("");
+  const [token, setToken] = React.useContext(TokenContext);
+
+  async function login(e) {
+    const body = {
+      username: username.current.value,
+      password: password.current.value,
+    };
+
+    const response = await fetch(`${api_base_url}/auth/login`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const responseBody = await response.json();
+
+    if (responseBody.success) {
+      setToken(responseBody.token);
+    }
+  }
+
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <p className="h4 text-center mb-4">Sign in</p>
+          <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+            Email
+          </label>
+          <input
+            type="email"
+            id="defaultFormLoginEmailEx"
+            className="form-control"
+            ref={username}
+          />
+          <br />
+          <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+            Password
+          </label>
+          <input
+            type="password"
+            id="defaultFormLoginPasswordEx"
+            className="form-control"
+            ref={password}
+          />
+          <div className="text-center mt-4">
+            <MDBBtn color="indigo" type="submit" onClick={login}>
+              Login
+            </MDBBtn>
+          </div>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 };
 
 export default Login;
